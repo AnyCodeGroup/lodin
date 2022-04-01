1b31d09fbc54d271024ec9eef6d116ee4468afe2
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-04-02 07:03:37 +0900

    27번 오류 해결

--- code_festivial/21-30/27.js
+++ code_festivial/21-30/27.js
@@ -1,3 +1,24 @@
+// const solution = function () {
+//   let a = prompt("이름");
+//   console.log(a);
+//   let b = prompt("점수");
+//   console.log(b);
+//   let keyArr = a.split(" ");
+//   console.log(keyArr);
+//   let valueArr = b.split(" ");
+//   console.log(valueArr);
+//   let result = {};
+//   for (let i = 0; i < a.length; i++) {
+//     result[keyArr[i]] = valueArr[i];
+//   }
+//   console.log(result)
+// }
+
+// undefined가 왜나오지
+// aaa bbb , 70 100 입력시
+// {aaa: '70', bbb: '100', undefined: undefined}
+
+
 const solution = function () {
   let a = prompt("이름");
   console.log(a);
@@ -8,12 +29,9 @@ const solution = function () {
   let valueArr = b.split(" ");
   console.log(valueArr);
   let result = {};
-  for (let i = 0; i < a.length; i++) {
+  for (let i = 0; i < keyArr.length; i++) {
     result[keyArr[i]] = valueArr[i];
   }
   console.log(result)
 }
-
-// undefined가 왜나오지
-// aaa bbb , 70 100 입력시
-// {aaa: '70', bbb: '100', undefined: undefined}
+// for문 배열을 a string 기준으로 해서 오류가 난것.... ㅠㅠ
