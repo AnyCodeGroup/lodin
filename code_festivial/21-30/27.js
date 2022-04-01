d3f47da48f15afc4b6a30c3b30ef37adefacbe0e
lcyljy <35355087+lcyljy@users.noreply.github.com>
2022-04-02 06:52:47 +0900

    Update 27.js

--- code_festivial/21-30/27.js
+++ code_festivial/21-30/27.js
@@ -1,9 +1,19 @@
-const solution = function (a, b) {
+const solution = function () {
+  let a = prompt("이름");
+  console.log(a);
+  let b = prompt("점수");
+  console.log(b);
   let keyArr = a.split(" ");
+  console.log(keyArr);
   let valueArr = b.split(" ");
-  let result = {}
+  console.log(valueArr);
+  let result = {};
   for (let i = 0; i < a.length; i++) {
-    result.keyArr[i] = valueArr[i];
+    result[keyArr[i]] = valueArr[i];
   }
   console.log(result)
 }
+
+// undefined가 왜나오지
+// aaa bbb , 70 100 입력시
+// {aaa: '70', bbb: '100', undefined: undefined}
